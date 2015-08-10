dcee3af47a143baa03204a630c5772de61c710ba
amitchen22 <amit.chen@checkmarx.com>
2015-08-10 12:33:21 +0300

    Update Example2.cpp

--- Beijing_1232_lines/Beijing/Beijing - cpptest/Example2.cpp
+++ Beijing_1232_lines/Beijing/Beijing - cpptest/Example2.cpp
@@ -1,10 +1,7 @@
 /* unexploitable strcpy #1 */
 
 /* This program contains a buffer overflow, but the overflowing data
-   isn't controlled by the attacker. Ideally, a scanner should either not
-   report a buffer overflow associated with this strcpy, or at most report
-   a problem with lower severity than a strcpy whose argument is attacker-
-   controlled.
+   is ghdf hgf hfg
 */
 
 # include <iostream> 
